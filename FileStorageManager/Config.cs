ca4285225a2a357d10bc7c3133560cade59e8cc3
edwardcordero <edwardcordero98@gmail.com>
2021-11-29 15:05:40 -0800

    * Config.cs: * tempkey.jwk: * WeatherForecast.cs: * home.component.ts: * WeatherForecastController.cs: * home.component.html: * counter.component.ts: * counter.component.html: * nav-menu.component.ts: * nav-menu.component.css: * nav-menu.component.html: * counter.component.spec.ts: * fetch-data.component.ts: * fetch-data.component.html:
    * appsettings.json: register page

---
+++ FileStorageManager/Config.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace AuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                // machine to machine client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5001/login" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5001/login" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                }
+            };
+        }
+    }
+}
