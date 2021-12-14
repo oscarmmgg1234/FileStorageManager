fa9190d3c2d56c276dc6ddc69ff1cfa397df4b6f
edwardcordero <edwardcordero98@gmail.com>
2021-12-13 22:44:31 -0800

    * Error.cshtml: * Error.cshtml.cs: * Index.cshtml: * _ViewImports.cshtml: * LoginViewModel.cs: * LoginInputModel.cs: * 20211202221005_initial.cs: * 20211202221005_initial.Designer.cs:
    * UserPrefs.xml: add views

---
+++ FileStorageManager/Models/Account/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace FileStorageManager.Models.Account
+{
+    public class LoginViewModel : LoginInputModel
+    {
+        public bool AllowRememberLogin { get; set; } = true;
+        public bool EnableLocalLogin { get; set; } = true;
+    }
+}
